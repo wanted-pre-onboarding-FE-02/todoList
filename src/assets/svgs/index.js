9ac44c6d579f4430e44144b1a61cecda433e9c51
doki333 <duckky333@gmail.com>
2022-05-05 01:08:54 +0900

    add function:toggle todo's done and remove todo

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,3 +1,4 @@
 export { ReactComponent as LogoImage } from './logo.svg'
 export { ReactComponent as CheckIcon } from './check.svg'
 export { ReactComponent as PlusIcon } from './plus.svg'
+export { ReactComponent as EditIcon } from './edit.svg'
--- src/pages/TodoList/TodoList.jsx
+++ src/pages/TodoList/TodoList.jsx
@@ -3,59 +3,67 @@ import styles from './TodoList.module.scss'
 import { PlusIcon, CheckIcon } from '../../assets/svgs/index'
 import { cx } from '../../styles/index'
 
-const CATEGORY = ['healthy', 'study', 'etc']
 const INITIAL_TODO = [
   {
     id: 1,
     content: 'do something',
     done: false,
-    category: 'etc',
   },
   {
     id: 2,
     content: 'play badminton',
     done: false,
-    category: 'exercise',
   },
   {
     id: 3,
     content: 'eat hamburger',
     done: false,
-    category: 'healthy',
   },
   {
     id: 4,
     content: 'learn typescript',
     done: false,
-    category: 'study',
   },
   {
     id: 5,
     content: 'learn typescript',
     done: false,
-    category: 'study',
   },
   {
     id: 6,
     content: 'learn typescript',
     done: false,
-    category: 'study',
   },
   {
     id: 7,
     content: 'learn typescript',
     done: false,
-    category: 'study',
   },
   {
     id: 8,
     content: 'learn typescript',
     done: false,
-    category: 'study',
   },
 ]
 
 function TodoList() {
+  const [todos, setTodos] = useState([...INITIAL_TODO])
+  const [text, setText] = useState('')
+
+  const onChangeTodoCheck = useCallback((e) => {
+    const CHECK_ID = parseInt(e.target.dataset.id, 10)
+    setTodos((todos) => todos.map((todo, index) => (todo.id === CHECK_ID ? { ...todo, done: !todo.done } : todo)))
+  }, [])
+
+  const onChangeText = (e) => {
+    setText(e.currentTarget.value)
+  }
+
+  const onRemoveTodo = (e) => {
+    const DELETE_ID = parseInt(e.target.dataset.id, 10)
+    setTodos((todos) => todos.filter((todo) => todo.id !== DELETE_ID))
+  }
+
   return (
     <section className={styles.todoWrapper}>
       <div className={styles.todoHeader}>
@@ -63,8 +71,25 @@ function TodoList() {
       </div>
       <div className='todoContent'>
         <div className={styles.todoList}>
-          <input type='text' />
+          <input type='text' value={text} onChange={onChangeText} />
           <h3 className={styles.todoTitle}>Today&#39;s tasks</h3>
+
+          <ul className={styles.todoInner}>
+            {todos.map((todo, index) => {
+              return (
+                <li className={styles.todoElement} key={`todo-${todo.id}`}>
+                  <div className={styles.checkboxWrapper}>
+                    <input type='checkbox' checked={todo.done} data-id={todo.id} onChange={onChangeTodoCheck} />
+                    <CheckIcon />
+                    <p>{todo.content}</p>
+                  </div>
+                  <button type='button' className={styles.todoDeleteBtn} data-id={todo.id} onClick={onRemoveTodo}>
+                    X
+                  </button>
+                </li>
+              )
+            })}
+          </ul>
         </div>
         <button type='button' className={styles.todoAddBtn} aria-label='Add Todo Button'>
           <PlusIcon />
