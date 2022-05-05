181d7e01f03c9a0915628099373ee9386057e180
changkeun <ckdrmsdk@gmail.com>
2022-05-05 13:15:09 +0900

    UI 구현 완료.

--- src/assets/svgs/index.js
+++ src/assets/svgs/index.js
@@ -1,2 +1,5 @@
 export { ReactComponent as LogoImage } from './logo.svg'
 export { ReactComponent as CheckIcon } from './check.svg'
+export { ReactComponent as Menu } from './menu.svg'
+export { ReactComponent as Search } from './search.svg'
+export { ReactComponent as Alarm } from './alarm.svg'
--- src/pages/TodoList/TodoList.jsx
+++ src/pages/TodoList/TodoList.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import styles from './TodoList.module.scss'
+import Header from '../../components/Header'
+import Title from '../../components/Title'
+import Category from '../../components/Category'
+import Task from '../../components/Task'
 
 function TodoList() {
-  return <div>test</div>
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Title label={`What's up Joy!`} isLarge />
+      <Category />
+      <Task />
+      {/* Button */}
+    </div>
+  )
 }
 
 export default TodoList
