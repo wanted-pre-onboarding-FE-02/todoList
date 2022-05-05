33e9e80d872e9d16019b244d822aad819e9fd9d5
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-05-05 11:54:14 +0900

    refactor Main컴포넌트 html구조변경

--- src/pages/TodoList/components/Main/Main.jsx
+++ src/pages/TodoList/components/Main/Main.jsx
@@ -9,13 +9,13 @@ function Main({ todos }) {
   return (
     <div className={classes.main}>
       <h1 className={classes.main__title}>What&apos;s up, Joy!</h1>
+      <h2 className={classes.main__category}>categories</h2>
       <div className={classes.slider}>
-        <h2 className={classes.main__category}>categories</h2>
         {/* <div className={classes.slider__content}>슬라이더</div> */}
         <InputFormContainer />
       </div>
+      <h2 className={classes.main__category}>Today&apos;s Tasks</h2>
       <div className={classes.list}>
-        <h2 className={classes.main__category}>Today&apos;s Tasks</h2>
         <ul className={classes.list__content}>
           {todos.map((item) => (
             <TodoItemContainer item={item} />
