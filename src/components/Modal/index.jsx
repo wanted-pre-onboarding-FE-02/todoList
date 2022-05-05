f7a7686e83ca68e361cf18894c14d54c41259ab1
doki333 <duckky333@gmail.com>
2022-05-06 00:13:30 +0900

    feat:modal close And fix modal position

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,13 +1,23 @@
+import { useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
 import styles from './Modal.module.scss'
 
 function Modal(props) {
-  const { text, handleChangeText } = props
+  const focusRef = useRef()
+  useEffect(() => {
+    focusRef.current.focus()
+  }, [])
+  const { text, handleChangeText, handleModal } = props
 
   return ReactDOM.createPortal(
     <div className={styles.addModal}>
-      <h3>Today&#39;s task is...</h3>
-      <input type='text' value={text} onChange={handleChangeText} className={styles.addTodoInput} />
+      <button type='button' className={styles.closeBtn} onClick={handleModal}>
+        X
+      </button>
+      <div className={styles.inputBlock}>
+        <h3>Today&#39;s task is...</h3>
+        <input type='text' value={text} ref={focusRef} onChange={handleChangeText} className={styles.addTodoInput} />
+      </div>
     </div>,
     document.getElementById('modal')
   )
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import styles from './Routes.module.scss'
 function App() {
   return (
     <div className={styles.app}>
+      <div id='modal' />
       <TodoApp />
     </div>
   )
