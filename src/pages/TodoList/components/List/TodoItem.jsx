c3c60ffca6319dd26c35113562eac476f098a706
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-05-05 14:02:33 +0900

    refactor: delete, Edit 버그 수정

--- src/pages/TodoList/components/List/TodoItem.jsx
+++ src/pages/TodoList/components/List/TodoItem.jsx
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types'
 import classes from './TodoItem.module.scss'
 
 import { ReactComponent as Check } from '../svgs/check.svg'
+import { useState } from 'react'
 
 function TodoItem({ item, toggle, onDelete }) {
+  const [isEditing, setIsEditing] = useState(false)
+  const [input, setInput] = useState(item.todo)
+
+  const textChangeHandler = (e) => {
+    setInput(e.target.value)
+    console.log(input)
+  }
+
+  const editingHandler = () => {
+    setIsEditing((prev) => !prev)
+  }
+
   const checkHandler = () => {
     toggle(item)
   }
@@ -25,7 +38,22 @@ function TodoItem({ item, toggle, onDelete }) {
       >
         <Check />
       </button>
-      <p className={`${classes.item__todo} ${item.checked ? '' : classes['notChecked--text']}`}>{item.todo}</p>
+      <p
+        className={`${classes.item__todo} ${item.checked ? '' : classes['notChecked--text']}`}
+        onClick={editingHandler}
+        role='presentation'
+      >
+        {!item.checked && isEditing ? (
+          <form type='submit'>
+            <input type='text' className={classes['item__todo--input']} onChange={textChangeHandler} value={input} />
+            <button type='button' className={classes.item__editBtn}>
+              Edit
+            </button>
+          </form>
+        ) : (
+          input
+        )}
+      </p>
       {item.checked && (
         <button type='button' className={classes.item__deleteBtn} onClick={clickDeleteHandler}>
           delete
