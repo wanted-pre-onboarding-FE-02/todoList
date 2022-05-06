9868ff989457aa6ee3bfbfd19b86bff082a6a65b
hyemin065 <p62517@gmail.com>
2022-05-07 00:15:16 +0900

    completed drag scroll

--- src/pages/TodoCategory/index.jsx
+++ src/pages/TodoCategory/index.jsx
@@ -1,30 +1,54 @@
 import styles from './TodoCategory.module.scss'
 import PropTypes from 'prop-types'
+import { useRef, useState } from 'react'
+import ScrollContainer from 'react-indiana-drag-scroll'
 
 export const CATEGORY = ['work', 'exercise', 'study', 'promise', 'etc']
 export default function TodoCategory({ todos }) {
   const completedTodo = todos.filter((item) => item.done === true)
 
+  const scrollRef = useRef(null)
+  const [isDrag, setIsDrag] = useState(false)
+  const [startX, setStartX] = useState()
+
+  const onDragStart = (e) => {
+    e.preventDefault()
+    setIsDrag(true)
+    setStartX(e.pageX + scrollRef.current.scrollLeft)
+  }
+
+  const onDragEnd = () => {
+    setIsDrag(false)
+  }
+
+  const onDragMove = (e) => {
+    if (isDrag) {
+      scrollRef.current.scrollLeft = startX - e.pageX
+    }
+  }
+
   return (
     <section className={styles.categoryWraper}>
       <h3>category</h3>
-      <ul className={styles.categoryInner}>
-        {CATEGORY.map((cate) => {
-          return (
-            <li key={`category-${cate}`}>
-              <span>{cate.length} Tasks</span>
-              <h4>{cate}</h4>
-              <div>
-                <p
-                  style={{
-                    width: `calc(100% / ${todos.length} * ${completedTodo.length} )`,
-                  }}
-                />
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <ScrollContainer className={styles.scrollContainer}>
+        <ul className={styles.categoryInner}>
+          {CATEGORY.map((cate) => {
+            return (
+              <li key={`category-${cate}`}>
+                <span>{cate.length} Tasks</span>
+                <h4>{cate}</h4>
+                <div>
+                  <p
+                    style={{
+                      width: `calc(100% / ${todos.length} * ${completedTodo.length} )`,
+                    }}
+                  />
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </ScrollContainer>
     </section>
   )
 }
