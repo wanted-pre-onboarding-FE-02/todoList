81ae2c93e32e129528732eaf8f55790a7c444894
changkeun <ckdrmsdk@gmail.com>
2022-05-07 19:00:16 +0900

    feat: 드래그앤드랍 기능 구현

--- src/pages/TodoList/TodoItem.jsx
+++ src/pages/TodoList/TodoItem.jsx
@@ -1,12 +1,34 @@
 import styles from './TodoItem.module.scss'
 import { CheckIcon } from '../../assets/svgs/index'
 import PropTypes from 'prop-types'
+import { cx } from 'styles'
+import { useEffect } from 'react'
 
-export default function TodoItem({ todo, handleToggle, handleRemove, handleEditMode }) {
+export default function TodoItem({
+  todo,
+  handleDragEnter,
+  handleDragOver,
+  handleDragStart,
+  handleDragEnd,
+  handleToggle,
+  handleRemove,
+  handleEditMode,
+  moveDown,
+  index,
+}) {
   const { id, text, done } = todo
 
   return (
-    <li className={styles.todoElement} key={`todo-${id}`}>
+    <li
+      onDragEnter={handleDragEnter}
+      onDragStart={handleDragStart}
+      onDragOver={handleDragOver}
+      onDragEnd={(e) => handleDragEnd(e, moveDown)}
+      draggable
+      className={cx(styles.todoElement, moveDown ? styles.moveDown : styles.moveUp)}
+      key={`todo-${id}`}
+      id={id}
+    >
       <div className={styles.checkboxWrapper}>
         <input type='checkbox' checked={done} data-id={id} onChange={handleToggle} />
         <CheckIcon />
@@ -31,6 +53,12 @@ TodoItem.propTypes = {
     // category: PropTypes.string.isRequired,
     // isLike: PropTypes.bool.isRequired,
   }),
+  index: PropTypes.number,
+  moveDown: PropTypes.bool,
+  handleDragEnter: PropTypes.func,
+  handleDragOver: PropTypes.func,
+  handleDragStart: PropTypes.func,
+  handleDragEnd: PropTypes.func,
   handleToggle: PropTypes.func,
   handleRemove: PropTypes.func,
   handleEditMode: PropTypes.func,
--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -1,20 +1,95 @@
+import { useCallback, useEffect, useState } from 'react'
 import styles from './TodoList.module.scss'
 import TodoItem from './TodoItem'
 import PropTypes from 'prop-types'
 
-export default function TodoList({ todos, handleToggle, handleEditMode, handleRemove }) {
+export default function TodoList({ todos, setTodos, handleToggle, handleEditMode, handleRemove }) {
+  const [todoList, setTodoList] = useState(todos)
+  const [startIndex, setStartIndex] = useState(-2)
+  const [nextIndex, setNextIndex] = useState(99)
+
+  const handleDragStart = useCallback(
+    (e) => {
+      e.stopPropagation()
+
+      setTimeout(() => {
+        e.target.style.display = 'none'
+        return true
+      }, 0)
+
+      const { id } = e.currentTarget
+
+      e.dataTransfer.setData('list_id', id)
+      const startIndex = todoList.findIndex((el) => el.id === Number(id))
+      setStartIndex(startIndex)
+    },
+    [todoList]
+  )
+
+  const handleDragEnter = useCallback(
+    (e) => {
+      e.preventDefault()
+      const { id } = e.currentTarget
+
+      const nextIndex = todoList.findIndex((el) => el.id === Number(id))
+      setNextIndex(nextIndex)
+    },
+    [todoList]
+  )
+
+  const handleDragOver = useCallback((e) => {
+    e.preventDefault()
+  }, [])
+
+  const handleDragEnd = useCallback(
+    (e) => {
+      if (e.target.style.display === 'none') {
+        e.target.style.display = 'flex'
+      }
+
+      const copiedList = [...todoList]
+      const startTodoEl = copiedList[startIndex]
+      copiedList.splice(startIndex, 1)
+
+      let nextTarget = nextIndex
+      if (nextIndex < startIndex) nextTarget += 1
+
+      copiedList.splice(nextTarget, 0, startTodoEl)
+      setNextIndex(99)
+      setStartIndex(-2)
+      setTodoList(copiedList)
+    },
+    [todoList, nextIndex, startIndex]
+  )
+
+  const handleDrop = useCallback((e) => {
+    const id = e.dataTransfer.getData('list_id')
+    const list = document.getElementById(id)
+    list.style.display = 'flex'
+  }, [])
+
+  console.log(todoList)
+
   return (
     <section className={styles.todoList}>
       <h3>Today&#39;s tasks</h3>
-
-      <ul className={styles.todoInner}>
-        {todos.map((todo) => (
+      <ul onDrop={handleDrop} onDragOver={(e) => e.preventDefault()} className={styles.todoInner}>
+        {todoList.map((todo, index) => (
           <TodoItem
+            index={index}
+            moveDown={nextIndex < index}
             todo={todo}
+            todoList={todoList}
+            setTodos={setTodos}
             key={`todo-${todo.id}`}
             handleToggle={handleToggle}
             handleEditMode={handleEditMode}
             handleRemove={handleRemove}
+            handleDragEnter={handleDragEnter}
+            handleDragOver={handleDragOver}
+            handleDragStart={handleDragStart}
+            handleDragEnd={handleDragEnd}
+            setTodoList={setTodoList}
           />
         ))}
       </ul>
@@ -33,6 +108,7 @@ TodoList.propTypes = {
       isLike: PropTypes.bool,
     })
   ),
+  setTodos: PropTypes.func,
   handleToggle: PropTypes.func,
   handleEditMode: PropTypes.func,
   handleRemove: PropTypes.func,
