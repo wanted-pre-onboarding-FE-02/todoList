792a90c90a8789f0069599ef67429bb3834eff05
AnJuHwan <wnghks0531@gmail.com>
2022-05-07 14:43:05 +0900

    fix: TodoCategory handleCategory prop-types error

--- src/pages/TodoCategory/index.jsx
+++ src/pages/TodoCategory/index.jsx
@@ -3,38 +3,36 @@ import styles from './TodoCategory.module.scss'
 // const CATEGORY = ['work', 'exercise', 'study', 'clean']
 const CATEGORY = [
   {
-    'title' : 'all',
-    'category' : ''
+    title: 'all',
+    category: '',
   },
   {
-    'title' : 'work',
-    'category' : 'red'
+    title: 'work',
+    category: 'red',
   },
   {
-    'title' : 'exercise',
-    'category' : 'orange'
+    title: 'exercise',
+    category: 'orange',
   },
   {
-    'title' : 'study',
-    'category' : 'yellow'
+    title: 'study',
+    category: 'yellow',
   },
   {
-    'title' : 'clean',
-    'category' : 'green'
+    title: 'clean',
+    category: 'green',
   },
 ]
+// eslint-disable-next-line react/prop-types
 export default function TodoCategory({ handleCategory }) {
   return (
     <section>
       <h3>category</h3>
       <ul className={styles.categoryInner}>
-        {CATEGORY.map(({title, category}) => {
+        {CATEGORY.map(({ title, category }) => {
           return (
             <li key={title}>
-              <button 
-                type="button" 
-                onClick={handleCategory} 
-                value={category}>
+              <button type='button' onClick={handleCategory} value={category}>
                 {title}
               </button>
             </li>
