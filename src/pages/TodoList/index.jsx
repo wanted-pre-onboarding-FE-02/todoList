7294d71e7a6589d892c83ef9c0cb3b380a4177c2
changkeun <ckdrmsdk@gmail.com>
2022-05-07 22:34:21 +0900

    fix: 커스텀 훅 분리

--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -1,65 +1,12 @@
-import { useCallback, useState } from 'react'
 import styles from './TodoList.module.scss'
 import TodoItem from './TodoItem'
 import PropTypes from 'prop-types'
+import useDragDrop from 'hooks/useDragDrop'
 
 export default function TodoList({ todos, setTodos, handleToggle, handleEditMode, handleRemove }) {
-  const [todoList, setTodoList] = useState(todos)
-  const [startId, setStartId] = useState(-1)
-  const [nextIndex, setNextIndex] = useState(Number.MAX_SAFE_INTEGER)
+  const [handleDragEnd, handleDragEnter, handleDragOver, handleDragStart, handleDrop, todoList, nextIndex] =
+    useDragDrop(todos)
 
-  const handleDragStart = useCallback((e) => {
-    e.stopPropagation()
-    const { id } = e.currentTarget
-
-    e.dataTransfer.setData('list_id', id)
-    setTimeout(() => {
-      e.target.style.display = 'none'
-      return true
-    }, 0)
-
-    setStartId(id)
-  }, [])
-
-  const handleDragEnter = useCallback(
-    (e) => {
-      e.preventDefault()
-      const { id } = e.currentTarget
-
-      const newNextIndex = todoList.findIndex((el) => el.id === Number(id))
-      if (newNextIndex === nextIndex) return
-
-      setNextIndex(newNextIndex)
-
-      const copiedList = [...todoList]
-      const startIndex = copiedList.findIndex((el) => el.id === Number(startId))
-      const startEl = copiedList.find((el) => el.id === Number(startId))
-
-      copiedList.splice(startIndex, 1)
-      copiedList.splice(newNextIndex, 0, startEl)
-      setTodoList(copiedList)
-    },
-    [todoList, startId, nextIndex]
-  )
-
-  const handleDragOver = useCallback((e) => {
-    e.preventDefault()
-  }, [])
-
-  const handleDragEnd = useCallback((e) => {
-    if (e.target.style.display === 'none') {
-      e.target.style.display = 'flex'
-    }
-
-    setNextIndex(Number.MAX_SAFE_INTEGER)
-    setStartId(-1)
-  }, [])
-
-  const handleDrop = useCallback((e) => {
-    const id = e.dataTransfer.getData('list_id')
-    const list = document.getElementById(id)
-    list.style.display = 'flex'
-  }, [])
   return (
     <section className={styles.todoList}>
       <h3>Today&#39;s tasks</h3>
@@ -67,7 +14,6 @@ export default function TodoList({ todos, setTodos, handleToggle, handleEditMode
         {todoList.map((todo, index) => {
           return (
             <TodoItem
-              index={index}
               move={nextIndex < index}
               todo={todo}
               todoList={todoList}
@@ -80,7 +26,6 @@ export default function TodoList({ todos, setTodos, handleToggle, handleEditMode
               handleDragOver={handleDragOver}
               handleDragStart={handleDragStart}
               handleDragEnd={handleDragEnd}
-              setTodoList={setTodoList}
             />
           )
         })}
