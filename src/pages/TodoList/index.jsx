57265c05b72060fd79d52599115b8a0e8ebb7774
Bo Hyeon Kim <rlaqhguse@gmail.com>
2022-05-07 11:38:32 +0900

    feat: 정렬함수 작성, 간단한 마크다운 작성

--- src/pages/TodoList/index.jsx
+++ src/pages/TodoList/index.jsx
@@ -1,22 +1,67 @@
-import styles from './TodoList.module.scss'
-import TodoItem from './TodoItem'
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
+import styles from './TodoList.module.scss'
+
+import FilterModal from 'components/FilterModal'
+import { ReactComponent as Filter } from '../../assets/svgs/filter.svg'
+import TodoItem from './TodoItem'
+
+import { sortByDateAsd, sortByDateDsd, sortByDic, sortByDone } from 'utils/sort'
+
+const FILTER_LIST = ['최신순', '과거순', '가나다순']
+
 export default function TodoList({ todos, handleToggle, handleEditMode, handleRemove }) {
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [selectedFilter, setSelectedFilter] = useState(FILTER_LIST[0])
+  const dateParsedTodos = todos.map((todo) => ({ ...todo, date: Date.parse(todo.date) }))
+  let listMarkup
+
+  const modalToggleHandler = () => {
+    setIsModalOpen((prev) => !prev)
+  }
+
+  const selectFilterHandler = (e) => {
+    const filter = e.target.innerText
+
+    switch (filter) {
+      case filter === FILTER_LIST[0]:
+        setSelectedFilter(FILTER_LIST[0])
+        break
+      case filter === FILTER_LIST[1]:
+        setSelectedFilter(FILTER_LIST[1])
+        break
+      case filter === FILTER_LIST[2]:
+        setSelectedFilter(FILTER_LIST[2])
+        break
+      default:
+        break
+    }
+  }
+  console.log(todos.sort((a, b) => sortByDateAsd(a, b)))
+
   return (
     <section className={styles.todoList}>
-      <h3>Today&#39;s tasks</h3>
+      {isModalOpen && <FilterModal onSelectFilter={selectFilterHandler} filterList={FILTER_LIST} />}
+      <div className={styles.header}>
+        <h3>Today&#39;s tasks</h3>
+        <button type='button' onClick={modalToggleHandler}>
+          <Filter className={styles.header__icon} />
+        </button>
+      </div>
 
       <ul className={styles.todoInner}>
-        {todos.map((todo) => (
-          <TodoItem
-            todo={todo}
-            key={`todo-${todo.id}`}
-            handleToggle={handleToggle}
-            handleEditMode={handleEditMode}
-            handleRemove={handleRemove}
-          />
-        ))}
+        {todos
+          .sort((a, b) => sortByDone(a, b))
+          .map((todo) => (
+            <TodoItem
+              todo={todo}
+              key={`todo-${todo.id}`}
+              handleToggle={handleToggle}
+              handleEditMode={handleEditMode}
+              handleRemove={handleRemove}
+            />
+          ))}
       </ul>
     </section>
   )
---
+++ src/utils/sort.js
@@ -0,0 +1,63 @@
+export const sortByDateAsd = (a, b) => {
+  // 오름차순
+  const dateA = Date.parse(a.date)
+  const dateB = Date.parse(b.date)
+
+  if (dateA < dateB) {
+    return -1
+  }
+
+  if (dateA > dateB) {
+    return 1
+  }
+
+  return 0
+}
+
+export const sortByDateDsd = (a, b) => {
+  // 내림차순
+  const dateA = Date.parse(a.date)
+  const dateB = Date.parse(b.date)
+
+  if (dateA < dateB) {
+    return 1
+  }
+
+  if (dateA > dateB) {
+    return -1
+  }
+
+  return 0
+}
+
+export const sortByDic = (a, b) => {
+  // 사전순
+  const textA = a.text
+  const textB = b.text
+
+  if (textA < textB) {
+    return -1
+  }
+
+  if (textA > textB) {
+    return 1
+  }
+
+  return 0
+}
+
+export const sortByDone = (a, b) => {
+  // 사전순
+  const doneA = a.done
+  const doneB = b.done
+
+  if (doneA < doneB) {
+    return -1
+  }
+
+  if (doneA > doneB) {
+    return 1
+  }
+
+  return 0
+}
